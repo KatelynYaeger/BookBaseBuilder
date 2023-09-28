065a28bdea01733fcbd52486a36bbc9e08a3b1eb
Katelyn Yaeger <knyaeger93@gmail.com>
2023-09-28 16:04:36 -0500

    isbn added

--- BookBaseBuilder/Book.cs
+++ BookBaseBuilder/Book.cs
@@ -14,24 +14,8 @@ namespace BookBaseBuilder
         public string Author { get; set; }
 		public string Genre { get; set; }
 		public string Characteristics { get; set; }
+		public string ISBN { get; set; }
 
-		//public enum Genre
-  //      {
-		//	Alternate History = 1,
-		//	Apocalyptic = 2,
-		//	Childrens = 3,
-		//	Fairytale = 4,
-		//	Fantasy = 5,
-		//	General Fiction = 6,
-		//	Gothic = 7,
-		//	Historical Fiction = 8,
-		//	Legal Thriller = 9,
-		//	Mystery = 10,
-		//	Romance = 11,
-		//	Science Fiction = 12,
-		//	Thriller = 13,
-		//	Young Adult = 14
-  //      }
     }
 }
 
--- BookBaseBuilder/BookRepository.cs
+++ BookBaseBuilder/BookRepository.cs
@@ -17,26 +17,27 @@ namespace BookBaseBuilder
 
         public IEnumerable<Book> GetAllBooks()
         {
-            return _conn.Query<Book>("SELECT BOOKID, TITLE, AUTHOR, GENRE, CHARACTERISTICS FROM ATTRIBUTES;");
+            return _conn.Query<Book>("SELECT BOOKID, TITLE, AUTHOR, GENRE, CHARACTERISTICS, ISBN FROM ATTRIBUTES;");
         }
 
         public Book GetBook(int id)
         {
-            return _conn.QuerySingle<Book>("SELECT BOOKID, TITLE, AUTHOR, GENRE, CHARACTERISTICS FROM ATTRIBUTES WHERE BOOKID = @id",
+            return _conn.QuerySingle<Book>("SELECT BOOKID, TITLE, AUTHOR, GENRE, CHARACTERISTICS, ISBN FROM ATTRIBUTES WHERE BOOKID = @id",
                 new { id = id });
         }
 
         public void UpdateBook(Book book)
         {
-            _conn.Execute("UPDATE attributes SET Title = @title, Author = @author, Genre = @genre, Characteristics = @characteristics WHERE BookID = @id",
-                new { title = book.Title, author = book.Author, genre = book.Genre, characteristics = book.Characteristics, id = book.BookID , });
+            _conn.Execute("UPDATE attributes SET Title = @title, Author = @author, Genre = @genre, Characteristics = @characteristics, ISBN = @isbn WHERE BookID = @id",
+                new { title = book.Title, author = book.Author, genre = book.Genre, characteristics = book.Characteristics, id = book.BookID , isbn = book.ISBN
+                });
 
         }
 
         public void InsertBook(Book bookToInsert)
         {
-            _conn.Execute("INSERT INTO attributes (TITLE, AUTHOR, GENRE, CHARACTERISTICS) VALUES (@title, @author, @genre, @characteristics);",
-    new { title = bookToInsert.Title, author = bookToInsert.Author, genre = bookToInsert.Genre, characteristics = bookToInsert.Characteristics });
+            _conn.Execute("INSERT INTO attributes (TITLE, AUTHOR, GENRE, CHARACTERISTICS, ISBN) VALUES (@title, @author, @genre, @characteristics, @ISBN);",
+    new { title = bookToInsert.Title, author = bookToInsert.Author, genre = bookToInsert.Genre, characteristics = bookToInsert.Characteristics, isbn = bookToInsert.ISBN });
 
         }
 
